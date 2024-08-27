c05e19b5f63fd4a83a3356c37939f4d55541ac1b
bracesproul <braceasproul@gmail.com>
2024-08-27 14:58:00 -0700

    cr

--- high-cardinality-tools/src/simple_graph.ts
+++ high-cardinality-tools/src/simple_graph.ts
@@ -16,13 +16,16 @@ function buildToolsWithFunctions(tools: StructuredToolParams[]) {
     // OpenAI's maximum tool description length is 1024 characters.
     const splitDescription = (desc: string) => {
       if (desc.length <= 1024) return desc;
-      return desc.substring(0, 1010).trim() + "...";
+      return desc.substring(0, 1020).trim() + "...";
     };
 
     return tool(
       (_) => {
         if (t.name === "_20202021_chinaindia_skirmishes") {
           return "China and India had a conflict in eastern Ladakh along the Line of Actual Control on September 7th, 2020.";
+        } else if (t.name === "fatahhamas_conflict") {
+          // Other question, correct answer.
+          return `The Fatah Hamas Conflict`;
         } else {
           // Selected the wrong tool, return the wrong answer.
           return "China and India had a conflict in eastern Ladakh along the Line of Actual Control on May 5, 2020.";
@@ -86,5 +89,8 @@ const workflow = new StateGraph(GraphAnnotation)
 
 export const graph = workflow.compile();
 
+// Descriptive question, LLM can relativity easily answer this question by calling the correct tool.
 const question =
   "On what date did China and India have a conflict in eastern Ladakh along the Line of Actual Control?";
+// Less descriptive, LLM has a very difficult time answering this question.
+const question2 = "What event involved Gilad Shalit?"; // Correct tool fatahhamas_conflict
