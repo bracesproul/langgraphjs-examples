5b7bc6d3add35f12291fdb0d8feccbb93ee75583
bracesproul <braceasproul@gmail.com>
2024-08-29 14:34:40 -0700

    added simple human in the loop graph

---
+++ human-in-the-loop/src/human_in_the_loop.ts
@@ -0,0 +1,45 @@
+import {
+  END,
+  START,
+  StateGraph,
+  MessagesAnnotation,
+} from "@langchain/langgraph";
+
+const nodeOne = (_: typeof MessagesAnnotation.State) => {
+  return {
+    messages: {
+      role: "user",
+      content: "Return value from __nodeOne__",
+    },
+  };
+};
+
+const nodeTwo = (_: typeof MessagesAnnotation.State) => {
+  return {
+    messages: {
+      role: "user",
+      content: "Return value from __nodeTwo__",
+    },
+  };
+};
+
+const nodeThree = (_: typeof MessagesAnnotation.State) => {
+  return {
+    messages: {
+      role: "user",
+      content: "Return value from __nodeThree__",
+    },
+  };
+};
+
+const workflow = new StateGraph(MessagesAnnotation)
+  .addNode("nodeOne", nodeOne)
+  .addNode("nodeTwo", nodeTwo)
+  .addNode("nodeThree", nodeThree)
+
+  .addEdge(START, "nodeOne")
+  .addEdge("nodeOne", "nodeTwo")
+  .addEdge("nodeTwo", "nodeThree")
+  .addEdge("nodeThree", END);
+
+export const graph = workflow.compile();
